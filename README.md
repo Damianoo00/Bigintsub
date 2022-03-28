949cfab83228549e96e2ac7c0f11fa87f7cf85e5
damianoo00 <damianoo00@Pavilion.localdomain>
2022-03-28 15:49:29 +0200

    Repair errors

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Bigintsub
+
+- To ran program
+```sh
+make
+./bigintsub [arg1] [arg2]
+```
+- To run test
+```sh
+make test
+```
+
--- testy.sh
+++ testy.sh
@@ -9,19 +9,26 @@ else
 fi
 }
 
-echo "Test 1:"
+echo "Test 1 ([+] - [+] 32):"
 echo $(make_test "./bigintsub 12345678901234567890123456789012 21098765432109876543210987654321" "-8753086530875308653087530865309")
-echo "Test 2: ([>+]-[+])"
+echo "Test 2: ([+] - [<+] 32)"
 echo $(make_test "./bigintsub 10000000000000000000000000000000 1" "9999999999999999999999999999999")
-echo "Test 3: ([+] - [>+] )"
+echo "Test 3: ([+] - [>+] 32)"
 echo $(make_test "./bigintsub 1 10000000000000000000000000000000" "-9999999999999999999999999999999")
 echo "Test 4 ([] - [] = 0):"
 echo $(make_test "./bigintsub 1 1" "0")
-echo "Test 5:"
-echo $(make_test "./bigintsub 0 1" "-1") 
-echo "Test 6: ([-] - [>+])"
-echo $(make_test "./bigintsub -1 10" "-11") 
-echo "Test 7: ([-] - [-])"
+echo "Test 5:([0] - [+]"
+echo $(make_test "./bigintsub 0 3" "-3") 
+echo "Test 6:([0] - [-]"
+echo $(make_test "./bigintsub 0 -6" "6") 
+echo "Test 7: ([-] - [>+])"
+echo $(make_test "./bigintsub -1 10" "-11")
+echo "Test 8: ([-] - [<+])"
+echo $(make_test "./bigintsub -3 2" "-5") 
+echo "Test 9: ([-] - [<-])"
 echo $(make_test "./bigintsub -1 -10" "9")
-echo "Test 8: ([0] - [0])"
+echo "Test 10: ([-] - [>-])"
+echo $(make_test "./bigintsub -10 -7" "-3")
+echo "Test 11: ([0] - [0])"
 echo $(make_test "./bigintsub 0 0" "0")
+
